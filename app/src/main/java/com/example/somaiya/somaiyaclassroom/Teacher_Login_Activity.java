91264e1486998ea17c3567a96c80c839bed0635f
arghyadeep99 <arghyadeep.d@somaiya.edu>
2018-07-15 16:29:02 +0530

    Minor FAQ changes

--- app/src/main/java/com/example/somaiya/somaiyaclassroom/Teacher_Login_Activity.java
+++ app/src/main/java/com/example/somaiya/somaiyaclassroom/Teacher_Login_Activity.java
@@ -45,7 +45,6 @@ import io.fabric.sdk.android.Fabric;
 
 public class Teacher_Login_Activity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
 
-    private static int backpress = 1;
     private GoogleSignInClient mGoogleSignInClient;
     private FirebaseAuth mAuth;
     private CardView mCourse;
@@ -187,7 +186,7 @@ public class Teacher_Login_Activity extends AppCompatActivity implements GoogleA
     }
 
 
-    public void openMainSignInProf(FirebaseUser user) {
+    public void openHome(FirebaseUser user) {
         finish();
         startActivity(new Intent(this, HomeActivity.class));
     }
@@ -213,7 +212,7 @@ public class Teacher_Login_Activity extends AppCompatActivity implements GoogleA
                 new OnCompleteListener<Void>() {
                     @Override
                     public void onComplete(@NonNull Task<Void> task) {
-                        openMainSignInProf(null);
+                        openHome(null);
 
                     }
                 });
