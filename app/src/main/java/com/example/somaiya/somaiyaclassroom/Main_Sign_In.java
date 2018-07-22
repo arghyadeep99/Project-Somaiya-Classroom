999667dbf0eee35962c8abccdfa90af593bd4a55
arghyadeep99 <arghyadeep.d@somaiya.edu>
2018-07-22 17:11:09 +0530

    Added spinning wheel progress dialog for login

--- app/src/main/java/com/example/somaiya/somaiyaclassroom/Main_Sign_In.java
+++ app/src/main/java/com/example/somaiya/somaiyaclassroom/Main_Sign_In.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.graphics.Paint;
 import android.graphics.drawable.GradientDrawable;
 import android.net.Uri;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -37,6 +38,7 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -130,11 +132,11 @@ public class Main_Sign_In extends AppCompatActivity implements GoogleApiClient.O
         super.onStart();
         // Check if user is signed in (non-null) and update UI accordingly.
         FirebaseUser currentUser = mAuth.getCurrentUser();
-        if(currentUser != null){
+        if (currentUser != null && mDatabase.toString().equals("Professors")) {
             Globals.tea = true;
             Globals.stu = false;
         }
-        openProfActivity(currentUser);
+            openProfActivity(currentUser);
     }
 
     @Override
@@ -178,7 +180,7 @@ public class Main_Sign_In extends AppCompatActivity implements GoogleApiClient.O
         Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
 
         // [START_EXCLUDE silent]
-        //showProgressDialog();
+        showProgressDialog();
         // [END_EXCLUDE]
 
         AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
@@ -235,5 +237,12 @@ public class Main_Sign_In extends AppCompatActivity implements GoogleApiClient.O
             }
                 finish();
         }
+    }
+    public void showProgressDialog(){
+        ProgressDialog progressDialog=new ProgressDialog(this);
+        progressDialog.setMessage("Logging in...");
+        progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        progressDialog.setIndeterminate(true);
+        progressDialog.show();
     }
 }
